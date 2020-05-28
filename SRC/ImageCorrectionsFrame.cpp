0d7cb612da8da02336c21aab59f3b3d466cb0bbb
Hleb Shypula <glebszypula1997@gmail.com>
2020-05-29 00:25:37 +0200

    Update ImageCorrectionsFrame.cpp

--- SRC/ImageCorrectionsFrame.cpp
+++ SRC/ImageCorrectionsFrame.cpp
@@ -17,21 +17,52 @@ void ImageCorrectionsFrame::image_BoxOnUpdateUI(wxUpdateUIEvent& event)
 void ImageCorrectionsFrame::color_Hexagon_BoxOnLeftDClick(wxMouseEvent& event)
 {
     // TODO: Implement color_Hexagon_BoxOnLeftDClick
+    Repaint();
 }
 
 void ImageCorrectionsFrame::chanel_choiceOnUpdateUI(wxUpdateUIEvent& event)
 {
     // TODO: Implement chanel_choiceOnUpdateUI
+    
 }
 
 void ImageCorrectionsFrame::slider_ChangeCoefficientOnScroll(wxScrollEvent& event)
 {
     // TODO: Implement slider_ChangeCoefficientOnScroll
+    if (m_Chanel_choice->GetSelection() == 0)
+    {
+        // Barwa
+    }
+    else if (m_Chanel_choice->GetSelection() == 1)
+    {
+        Brightness(m_Slider_ChangeCoefficient->GetValue() - 100);
+        Repaint();
+    }
+    else if (m_Chanel_choice->GetSelection() == 2)
+    {
+        // Nasycenie
+    }
+    else if (m_Chanel_choice->GetSelection() == 3)
+    {
+        Contrast(m_Slider_ChangeCoefficient->GetValue() - 100);
+        Repaint();
+    }
 }
 
 void ImageCorrectionsFrame::slider_MixImagesOnScroll(wxScrollEvent& event)
 {
     // TODO: Implement slider_MixImagesOnScroll
+
+    /*wxColor toChange(254, 0, 0);
+    wxColor newColor(0, 0, 255);
+    unsigned char toR = toChange.Red();
+    unsigned char toG = toChange.Green();
+    unsigned char toB = toChange.Blue();
+    unsigned char newR = newColor.Red();
+    unsigned char newG = newColor.Green();
+    unsigned char newB = newColor.Blue();
+    Img_Cpy.Replace(toR, toG, toB, newR, newG, newB);
+    Repaint();*/
 }
 
 void ImageCorrectionsFrame::menu_File_OpenOnMenuSelection(wxCommandEvent& event)
@@ -40,11 +71,14 @@ void ImageCorrectionsFrame::menu_File_OpenOnMenuSelection(wxCommandEvent& event)
     std::shared_ptr<wxFileDialog> Dialog(new wxFileDialog(this, _("Otwórz"), _(""), _(""), _("JPEG files (*.jpg)|*.jpg"), wxFD_OPEN));
     if (Dialog->ShowModal() == wxID_OK)
     {
+        Img_Org.AddHandler(new wxJPEGHandler);
+        Img_Org.AddHandler(new wxPNGHandler);
+        Img_Org.LoadFile(Dialog->GetPath());
         Img_Cpy.AddHandler(new wxJPEGHandler);
         Img_Cpy.AddHandler(new wxPNGHandler);
         Img_Cpy.LoadFile(Dialog->GetPath());
     }
-    if (Img_Cpy.IsOk())
+    if (Img_Org.IsOk() && Img_Cpy.IsOk())
         Repaint();
 }
 
@@ -64,7 +98,6 @@ void ImageCorrectionsFrame::menu_File_SaveOnMenuSelection(wxCommandEvent& event)
         }
         Repaint();
     }
-
 }
 
 void ImageCorrectionsFrame::menu_File_ExitOnMenuSelection(wxCommandEvent& event)
@@ -82,8 +115,52 @@ void ImageCorrectionsFrame::Repaint()
 {
     wxBitmap bitmap(Img_Cpy);          // Tworzymy tymczasowa bitmape na podstawie Img_Cpy
     wxClientDC dc(m_Image_Box);   // Pobieramy kontekst okna
-    wxBufferedDC buffDC(&dc);
-    buffDC.Clear();
-    m_Image_Box->DoPrepareDC(buffDC); // Musimy wywolac w przypadku wxScrolledWindow, zeby suwaki prawidlowo dzialaly
-    buffDC.DrawBitmap(bitmap, 0, 0, true); // Rysujemy bitmape na kontekscie urzadzenia
+    m_Image_Box->DoPrepareDC(dc); // Musimy wywolac w przypadku wxScrolledWindow, zeby suwaki prawidlowo dzialaly
+    dc.DrawBitmap(bitmap, 0, 0, true); // Rysujemy bitmape na kontekscie urzadzenia
+}
+
+void ImageCorrectionsFrame::Contrast(int value)
+{
+    Img_Cpy = Img_Org.Copy();
+    int size = Img_Org.GetWidth() * Img_Org.GetHeight() * 3;
+    unsigned char* Img_Data = Img_Cpy.GetData();
+
+    double contrast = (double(value) + 100) / (100.0001 - value);
+
+    for (int i = 0; i < size; i++)
+    {
+        int tmp = (Img_Data[i] - 255.0 / 2) * contrast + 255.0 / 2;
+
+        if (tmp > 255)
+            tmp = 255;
+        else if (tmp < 0)
+            tmp = 0;
+
+        Img_Data[i] = tmp;
+    }
+}
+
+void ImageCorrectionsFrame::Brightness(int value)
+{
+    Img_Cpy = Img_Org.Copy();
+    int size = Img_Org.GetWidth() * Img_Org.GetHeight() * 3;
+    unsigned char* Img_data = Img_Cpy.GetData();
+
+    int temp;
+
+    for (int i = 0; i < size; i++)
+    {
+        temp = Img_data[i] + value;
+
+        if (temp > 255)
+        {
+            temp = 255;
+        }
+        else if (temp < 0)
+        {
+            temp = 0;
+        }
+
+        Img_data[i] = temp;
+    }
 }
